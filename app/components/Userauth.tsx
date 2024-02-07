9780ba4973ac10d62266bf73118180fe549e15cc
Mohamad44p <mohamad44p@yahoo.com>
2024-02-07 23:22:16 +0200

    Fix afterSignOutUrl

--- app/components/Userauth.tsx
+++ app/components/Userauth.tsx
@@ -1,4 +1,4 @@
-import { UserButton } from "@clerk/nextjs";
+import { RedirectToSignUp, UserButton } from "@clerk/nextjs";
 import { Button } from '@/components/ui/button';
 import Link from "next/link";
 import { useUser } from "@clerk/nextjs";
@@ -10,7 +10,8 @@ export default function Userauth() {
     <>
       {user ? (
         <div>
-          <UserButton />
+          <UserButton afterSignOutUrl="/sign-in" />
+        
         </div>
       ) : (
         <div className="justify-center items-center gap-5 hidden  lg:flex 2xl:ml-16">
